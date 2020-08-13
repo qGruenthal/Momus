53dc2fe616bac988324075ecf2deee38b5e9e8e0
Quinten Cisneros-Gruenthal <quintengruenthal@gmail.com>
2020-08-13 16:27:23 -0700

    New Commandline Output

--- commandline_interface.c
+++ commandline_interface.c
@@ -1,6 +1,21 @@
 #include "commandline_interface.h"
 
 void
+simple_output_of_test (SCM test)
+{
+  SCM name = scm_list_ref (test, scm_from_int8 (0));
+  SCM passed = scm_list_ref (test, scm_from_int8 (1));
+  SCM value = scm_list_ref (test, scm_from_int8 (2));
+
+  int p = scm_to_bool (SCM_CDR (passed));
+  printf ("%s %s for %d\n"
+          , p ? "Passed" : "Failed"
+          , scm_to_locale_string (SCM_CDR (name))
+          , p ? scm_to_int (SCM_CDR (value)) : 0
+          );
+}
+
+void
 display_test ()
 {
   printf ("Display Test\n");
@@ -10,6 +25,7 @@ void
 scm_init_commandline_interface ()
 {
   scm_c_define_gsubr ("display-test", 0, 0, 0, display_test);
+  scm_c_define_gsubr ("simple-output-of-test", 1, 0, 0, simple_output_of_test);
 
   scm_c_eval_string("(add-hook! critique display-test)");
 }
--- commandline_interface.h
+++ commandline_interface.h
@@ -4,6 +4,8 @@
 #ifndef COMMANDLINE_INTERFACE_H
 #define COMMANDLINE_INTERFACE_H
 
+void simple_output_of_test (SCM test);
+
 void scm_init_commandline_interface ();
 
 #endif
